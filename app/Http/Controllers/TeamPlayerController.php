bc35bee4c13f583ff773956837ed20cb69a10e4d
Jacob Ekanem <prometheus6001@gmail.com>
2020-06-15 13:08:52 -0300

    Initial commit

---
+++ app/Http/Controllers/TeamPlayerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\TeamPlayer;
+use Illuminate\Http\Request;
+
+class TeamPlayerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\TeamPlayer  $teamPlayer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(TeamPlayer $teamPlayer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\TeamPlayer  $teamPlayer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(TeamPlayer $teamPlayer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\TeamPlayer  $teamPlayer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, TeamPlayer $teamPlayer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\TeamPlayer  $teamPlayer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(TeamPlayer $teamPlayer)
+    {
+        //
+    }
+}
---
+++ database/factories/TeamPlayerFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\TeamPlayer;
+use Faker\Generator as Faker;
+
+$factory->define(TeamPlayer::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});

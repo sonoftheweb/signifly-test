a2fd210e25a8b6f1d59dbb520c6bcb49fbccf816
Jacob Ekanem <prometheus6001@gmail.com>
2020-06-23 02:22:51 -0300

    Added application key generation to setup.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="Description" content="My implementation of a test given by Signifly. Author: Jacob Ekanem, Backend - PHP, Frontend - JS, HTML, CSS.">
 
-        <title>Laravel</title>
+        <title>Signifly Foosball Tournament</title>
         <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;700&family=Roboto:wght@400;500&display=swap" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}"/>
     </head>
